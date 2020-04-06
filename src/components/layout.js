ebf2ee5795f1bf53ffdd35d49d23f2fc52d0af14
James Marshall <james.marshall@eurocamp.com>
2020-04-06 11:17:24 +0100

    Added site-wide social sharing to footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,9 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import Navigation from "../components/navigation"
 import 'prismjs/themes/prism-okaidia.css';
 
+import { FaTwitter, FaFacebook, FaGooglePlus, FaEnvelope, FaLinkedin } from "react-icons/fa";
+import { ShareButtonRoundSquare, ShareBlockStandard } from "react-custom-share";
+
 export default ({ children }) => {
   const data = useStaticQuery(
     graphql`
@@ -15,6 +18,19 @@ export default ({ children }) => {
       }
     `
   )
+  const shareBlockProps = {
+    url: "https://blog.jamesmarshall.org",
+    button: ShareButtonRoundSquare,
+    buttons: [
+      { network: "Linkedin", icon: FaLinkedin },
+      { network: "Twitter", icon: FaTwitter },
+      { network: "Facebook", icon: FaFacebook },
+      { network: "GooglePlus", icon: FaGooglePlus },
+      { network: "Email", icon: FaEnvelope }
+    ],
+    text: `Take a look at this guys blog.`,
+    longtext: `Take a look at this guys blog. He's got some interesting posts you might be interested in.`
+  };
   return (
     <div className="site-wrapper">
       <header className="site-header">
@@ -25,7 +41,8 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; 2020 Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
+        <ShareBlockStandard {...shareBlockProps} />
+        <p>&copy; 2020 Delog &bull; Crafted with love by <a href="https://w3layouts.com">W3Layouts</a>, modified with intent by <a href="/">James Marshall</a></p>
       </footer>
     </div>
   )
